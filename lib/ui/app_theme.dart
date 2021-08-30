c851a5d9ac2d4cc76f2ff7c16c8127e04b903644
pouya <pouyarezaei810@gmail.com>
2021-08-30 10:48:40 +0430

    add app themes

---
+++ lib/ui/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const String themeKey = "selectedThemeIndex";
+  static final darkTheme = ThemeData(colorScheme: ColorScheme.dark());
+  static final lightTheme = ThemeData(colorScheme: ColorScheme.light());
+}
