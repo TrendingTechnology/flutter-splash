4f9b7f04926e6ec47a3be9790c5e3e38a941b799
pouya <pouyarezaei810@gmail.com>
2021-08-31 23:06:09 +0430

    add title and description

--- lib/ui/page/collection_page.dart
+++ lib/ui/page/collection_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_splash/ui/widget/collection_item.dart';
 
 class CollectionPage extends StatefulWidget {
   const CollectionPage({Key? key}) : super(key: key);
@@ -10,10 +11,41 @@ class CollectionPage extends StatefulWidget {
 class _CollectionPageState extends State<CollectionPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("CollectionPage"),
-      ),
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(32, 32, 0, 0),
+          child: Text(
+            "Collections",
+            style: TextStyle(
+                color: Colors.black,
+                fontFamily: "DancingScript",
+                fontSize: 32,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(32, 4, 0, 0),
+          child: Text(
+            "Explore the world through collections of beautiful photos free to use under the Unsplash License.",
+            style: TextStyle(
+              color: Colors.grey.shade600,
+              fontFamily: "Lato",
+              fontSize: 16,
+            ),
+            maxLines: 3,
+          ),
+        ),
+        ListView(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          children: [
+            CollectionItem(),
+            CollectionItem(),
+            CollectionItem(),
+          ],
+        )
+      ],
     );
   }
 }
